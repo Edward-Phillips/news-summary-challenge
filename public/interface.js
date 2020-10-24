0381783ff91623d6ab884efa9e37b62b252a7760
Edward Phillips <edd0ddi0@gmail.com>
2020-10-24 17:58:32 +0100

    use await instead of .then

--- public/interface.js
+++ public/interface.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', async function() {
-        var papers;
         var api_key = thing['PERSONAL_API_KEY'];
         var api_url = `http://content.guardianapis.com/search?api-key=${api_key}&show-fields=thumbnail,headline,bodyText,shortUrl,thumbnail`
-        getData(api_url).then(data=> {
-          papers = new newsReel(newsItem,data.response.results);
-          papers.articleParser();
-          displayer = new Displayer(papers.newsPieces);
-          displayer.displayArticles();
-          return data.results;
-        })
+        const query = await getData(api_url);
+        const { results } = query.response;
+        const papers = new newsReel(newsItem, results);
+        const newsPieces = papers.articleParser();
+        const displayer = new Displayer(newsPieces);
+        displayer.displayArticles();
 })
 
